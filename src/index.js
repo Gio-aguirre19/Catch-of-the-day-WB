12c537f81d951ab804fdd46419899017a1bb0e17
Giovani Aguirre <aguirregio19@gmail.com>
2018-03-05 23:26:42 -0800

    Made first componenet and added css to index file for live reload

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 // let's go!
+// 1) npm install 2)npm start
+import React from 'react';
+import { render } from 'react-dom';
+import StorePicker from './components/StorePicker';
+import "./css/style.css";
+
+
+render(<StorePicker /> , document.querySelector('#main'));
