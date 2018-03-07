56bbaed90bd0afd3cdb8f7b30b2c3c4ea461e01a
Giovani Aguirre <aguirregio19@gmail.com>
2018-03-06 16:11:27 -0800

    Learning about constructors, binding, and refs

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,11 +2,20 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  // New way to make refs
+  formInput = React.createRef();
+
+  // This syntex is so contructors and binding is not needed
+  goToStore = (e) => {
+    e.preventDefault();
+    console.log(this)
+  }
+
   render(){
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
-        <input type="text" required defaultValue={getFunName()} />
+        <input type="text" ref={this.formInput} required defaultValue={getFunName()} />
         <button type="submit">Visit Store</button>
       </form>
     )
